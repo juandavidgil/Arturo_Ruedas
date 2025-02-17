9d576892d14e40ddaeef11f33dc07efb6c25dc02
juandavidgil <juandavidgil0911@gmail.com>
2025-02-17 15:47:38 -0600

    creacion de carpeta proyecto

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image, ImageBackground, Button} from 'react-native';
+import  React, {useState}  from 'react';
+//import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+//constante para imagen de fondo
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>buena buena mi ppa</Text>
+      
+      
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
+/* const registroNombre = () => {
+  const [text, setText] = useState('');
+  return(
+
+    <TextInput
+    placeholder='Nombre'
+    onChangeText={newText => setText(newText)}
+    defaultValue={text}
+    />
+  
+  )} */
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#B2BEBF',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+
