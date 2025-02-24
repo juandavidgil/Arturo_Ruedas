3cf0a99b529a329ffcbc0d6ee4cc3ca6fc0f0731
juandavidgil <juandavidgil0911@gmail.com>
2025-02-24 16:06:13 -0600

    proceso navegar pantallas

---
+++ App.js
@@ -0,0 +1,131 @@
+//import * as React from 'react';
+import { View, TextInput, Text, Button, StyleSheet,  } from 'react-native';
+import { Button } from '@react-navigation/elements';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStaticNavigation, useNavigation, navigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import React, { useState } from 'react';
+
+
+
+//-----------------------------FUNCIONES Y PANTALLAS---------------------------------------------------------------//
+
+function RegistroPantalla(){
+  const [nombre, setNombre] = useState('');
+  const [telefono, setTelefono] = useState('');
+  const [correo, setCorreo] =useState('');
+  const [contraseña, setContraseña] = useState('');
+  const presionarBoton = () => {
+  // para que al presionar el boton de registrar se envien los datos en la consola
+  console.log('Nombre:', nombre);
+  console.log('Telefono:', telefono);
+  console.log('Correo:', correo);
+  console.log('Contraseña:', contraseña);
+  
+  const navigation = useNavigation();
+
+  };
+  return(
+    <View style={styles.container}>
+    <Text style={styles.tituloRegistrarse} >REGISTRARSE</Text>
+    
+    <TextInput
+      style={styles.input}
+      placeholder="Nombre"
+      value={nombre}
+      onChangeText={setNombre}
+    />
+    <TextInput
+      style={styles.input}
+      placeholder="Telefono"
+      value={telefono}
+      onChangeText={setTelefono}
+    />
+    <TextInput
+      style={styles.input}
+      placeholder="Correo"
+      keyboardType="email-address"
+      value={correo}
+      onChangeText={setCorreo}
+    />
+    <TextInput
+      style={styles.input}
+      placeholder="Contraseña"
+      secureTextEntry={true}
+      value={contraseña}
+      onChangeText={setContraseña}
+    />
+    {/* toca cambiar boton a touchableOpacity para poder agragar estilos */}
+    <Button  style={styles.BotonRegistrarse} title="Registrarse" onPress={presionarBoton} />
+
+    < Button onPress={()=> navigation.navigate('InicioSesion')}> Iniciar Sesion</Button> 
+    
+  </View>
+  )
+} 
+
+function InicioSesionPantalla(){
+  return(
+    <View>
+      <Text>hola</Text>
+    </View>
+  )
+}
+
+
+const Stack = createNativeStackNavigator();
+
+function RootStack(){
+  return(
+    <Stack.Navigator initialRouteName="Registro">
+    <Stack.Screen name="Registro"
+     component={RegistroPantalla}
+     options={{title: "supapa"}}/> 
+    <Stack.Screen name="InicioSesion"
+     component={InicioSesionPantalla}/>
+    </Stack.Navigator>
+  );
+}
+
+
+
+
+//--------------------ESTO VEMOS EN PANTALLA-----------------------------------------------------------------------------//
+export default function App() {
+
+return(
+  <NavigationContainer>
+    <RootStack/>
+  </NavigationContainer>
+)
+
+
+}
+
+//-------------------  ESTILOS--------------------------------------------------------------------------------------------//    
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 30,
+    backgroundColor: '#B5B8B1'
+  },
+  input: {
+    height: 80,
+    borderColor: '#ccc',
+    backgroundColor : 'white',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingLeft: 15,
+    paddingBottom: 50,
+    borderRadius: 20,
+},
+tituloRegistrarse: {
+  fontSize: 30,
+  color: 'white',
+  marginBottom: 100,
+  marginLeft: 70,
+ 
+},
+}); 
