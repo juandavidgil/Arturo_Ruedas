4a4b4d28339dcb229b9593c222cbe6357aa0a505
juandavidgil <juandavidgil0911@gmail.com>
2025-02-25 16:06:59 -0600

    LISTO P R IS

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-//import * as React from 'react';
-import { View, TextInput, Text, StyleSheet, TouchableOpacity } from 'react-native';
+//import React  from 'react';
+import { ImageBackground ,View, TextInput, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 import { Button } from '@react-navigation/elements';
 import { NavigationContainer } from '@react-navigation/native';
@@ -15,22 +15,35 @@ import React, { useState } from 'react';
 
 
 //-----------------------------PANTALLA PRESENTACION-----------------------------------------------//
+
+const image = {uri: 'https://cdn.leonardo.ai/users/ec93ea68-428d-4597-b04a-4c97d668081f/generations/a39d4587-7a1c-4782-81d8-f71744017f3a/Leonardo_Phoenix_Four_distinct_bicycle_wheels_a_sleek_narrow_3.jpg?w=512'}
 function PresentacionPantalla(){
   const navigation = useNavigation();
   return(
-    <View style={styles.container}>
-      <TouchableOpacity onPress={()=> navigation.navigate('InicioSesion')}>
+  <SafeAreaProvider>
+    <SafeAreaView style={styles.containerPresentacion} edges={['left', 'right']}>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        
+      <Text style={styles.tituloPresentacion}>ARTURO RUEDAS</Text>
+      
+      <View style={styles.containerBotonesPresenatacion}>
+      <TouchableOpacity style={styles.Boton} onPress={()=> navigation.navigate('InicioSesion')}>
         <Text>
           Iniciar Sesion
         </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={()=> navigation.navigate('Registro')}>
+      <TouchableOpacity style={styles.Boton} onPress={()=> navigation.navigate('Registro')}>
         <Text>
           Registrarse
         </Text>
       </TouchableOpacity>
-    </View>
+
+      </View>
+      </ImageBackground>
+    </SafeAreaView>
+  </SafeAreaProvider>
+    
   )
 }
 
@@ -41,8 +54,8 @@ function RegistroPantalla(){
   const [telefono, setTelefono] = useState('');
   const [correo, setCorreo] =useState('');
   const [contraseña, setContraseña] = useState('');
-  const presionarBoton = () => {
   // para que al presionar el boton de registrar se envien los datos en la consola
+  const presionarBotonRegistro = () => {
   console.log('Nombre:', nombre);
   console.log('Telefono:', telefono);
   console.log('Correo:', correo);
@@ -50,7 +63,7 @@ function RegistroPantalla(){
   };
   return(
     <View style={styles.container}>
-    <Text style={styles.tituloRegistrarse} >REGISTRARSE</Text>
+    <Text style={styles.titulo} >REGISTRARSE</Text>
     
     <TextInput
       style={styles.input}
@@ -78,10 +91,13 @@ function RegistroPantalla(){
       value={contraseña}
       onChangeText={setContraseña}
     />
-    {/* toca cambiar boton a touchableOpacity para poder agragar estilos */}
-    <Button  style={styles.BotonRegistrarse} title="Registrarse" onPress={presionarBoton} />
+    
+    {/* usamos TouchableOpacity como boton para poder darle estilo */}
+    <TouchableOpacity style={styles.BotonSesionRegistro} title="Registrarse" onPress={presionarBotonRegistro}>
+      <Text>Registrarse</Text>
+    </TouchableOpacity> 
 
-    <Button onPress={()=> navigation.navigate('InicioSesion')}> Iniciar Sesion</Button> 
+    
     
   </View>
   )
@@ -90,28 +106,57 @@ function RegistroPantalla(){
 
 //-----------------------------------PANTALLA INCIO SESION--------------------------------------------//
 function InicioSesionPantalla(){
+  const navigation = useNavigation();
+  const [correo, setCorreo] =useState('');
+  const [contraseña, setContraseña] = useState('');
+  // para que al presionar el boton de registrar se envien los datos en la consola
+  const presionarBotonSesion = () => {
+  console.log('Correo:', correo);
+  console.log('Contraseña:', contraseña);
+  }
   return(
-    <View>
+    <View style={styles.container}>
+      <Text style={styles.titulo}>INICIAR SESION</Text>
       <TextInput
       style={styles.input}
+      placeholder='Correo Electronico'
+      keyboardType='email-address'
+      value={correo}
+      onChangeText={setCorreo}
       />
+      <TextInput
+      style={styles.input}
+      placeholder='Contraseña'
+      secureTextEntry={true}
+      value={contraseña}
+      onChangeText={setContraseña}
+      />
+      <TouchableOpacity style={styles.BotonSesionRegistro} onPress={(presionarBotonSesion)}>
+        <Text>
+          Iniciar Sesion
+        </Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
 
-const Stack = createNativeStackNavigator();
 
+
+//-----------------------------------FUNCION PARA MOSTRAR PANTALLAS-------------------------------------//
+const Stack = createNativeStackNavigator();
 function RootStack(){
   return(
-    <Stack.Navigator initialRouteName="Registro">
+    <Stack.Navigator initialRouteName="Presentacion">
+    
     <Stack.Screen name="Presentacion"
     component={PresentacionPantalla}
     options={{title: "Presentacion"}}/>
     
-   <Stack.Screen name="Registro"
+    <Stack.Screen name="Registro"
      component={RegistroPantalla}
      options={{title: "Registro"}}/>  
+    
     
     <Stack.Screen name="InicioSesion"
      component={InicioSesionPantalla}
@@ -123,7 +168,7 @@ function RootStack(){
 
 
 
-//--------------------ESTO VEMOS EN PANTALLA-----------------------------------------------------------------------------//
+//--------------------ESTO VEMOS EN PANTALLA---------------------------------------------------------------//
 export default function App() {
 
 return(
@@ -135,13 +180,43 @@ return(
 
 }
 
-//-------------------  ESTILOS--------------------------------------------------------------------------------------------//    
+//-------------------  ESTILOS-------------------------------------------------------------------------------//    
 const styles = StyleSheet.create({
-  container: {
+
+  //pantalla presentacion
+  containerPresentacion: {
     flex: 1,
+    flex:1,
+    justifyContent: 'center',
+   
+    backgroundColor: '#D0D0D0'
+  },
+  image: {
+    flex: 1,
+    opacity: 1,
+    justifyContent: 'center',
+  },
+  tituloPresentacion:{
+    fontSize: 30,
+    color: 'white',
+    marginBottom: 80,
+    marginTop:20,
+    marginLeft: 80,
+    fontWeight:900,
+  },
+  containerBotonesPresenatacion:{
+    padding: 50,
+    marginTop: 400,
+    marginLeft:40,
+    
+  },
+
+  //pantalla registro e inicio de sesion
+  container: {
+    flex:1,
     justifyContent: 'center',
     padding: 30,
-    backgroundColor: '#B5B8B1'
+    backgroundColor: '#D0D0D0'
   },
   input: {
     height: 80,
@@ -153,11 +228,33 @@ const styles = StyleSheet.create({
     paddingBottom: 50,
     borderRadius: 20,
 },
-tituloRegistrarse: {
+titulo: {
+  
   fontSize: 30,
   color: 'white',
-  marginBottom: 100,
-  marginLeft: 70,
+  marginBottom: 80,
+  marginTop:20,
+  marginLeft: 60,
+  fontWeight:900,
  
 },
+Boton: {
+  alignItems: 'center',
+  margin:10,
+  width: 210,
+  padding:10,
+  borderRadius:30,
+  backgroundColor: '#FFFFFF' ,
+},
+BotonSesionRegistro:{
+  alignItems: 'center',
+  margin:10,
+  marginLeft:70,
+  width: 210,
+  padding:10,
+  borderRadius:30,
+  backgroundColor: '#FFFFFF' ,
+}
+
+
 }); 
